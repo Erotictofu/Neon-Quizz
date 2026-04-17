6e2a9cbbce58ce28941fe76bad4e59b3ebefdc4f
Erotictofu <matthieuvohy@gmail.com>
2026-04-17 20:11:35 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,39 +5,81 @@ const io = require('socket.io')(http);
 const axios = require('axios');
 const path = require('path');
 
-// On dit au serveur que les fichiers sont à la racine
 app.use(express.static(__dirname));
 
-// CETTE LIGNE EST LA CORRECTION : on pointe directement sur index.html
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 let players = {};
 let currentQuestion = null;
+let timeLeft = 30;
+let timerInterval = null;
 
 async function chargerNouvelleQuestion() {
     try {
         const response = await axios.get('https://the-trivia-api.com/v2/questions?limit=1');
         const q = response.data[0];
+        
         currentQuestion = {
             text: q.question.text,
             choices: [...q.incorrectAnswers, q.correctAnswer].sort(() => Math.random() - 0.5),
             correct: q.correctAnswer
         };
+
+        timeLeft = 30;
         io.emit('nextQuestion', currentQuestion);
+        startTimer();
     } catch (error) {
         setTimeout(chargerNouvelleQuestion, 2000);
     }
 }
 
+function startTimer() {
+    clearInterval(timerInterval);
+    timerInterval = setInterval(() => {
+        timeLeft--;
+        io.emit('timerUpdate', timeLeft);
+        if (timeLeft <= 0) {
+            clearInterval(timerInterval);
+            io.emit('timeUp', currentQuestion.correct);
+            setTimeout(chargerNouvelleQuestion, 4000);
+        }
+    }, 1000);
+}
+
 io.on('connection', (socket) => {
     socket.on('joinGame', (username) => {
-        players[socket.id] = { username, score: 0 };
-        if (Object.keys(players).length === 1 && !currentQuestion) chargerNouvelleQuestion();
-        else if (currentQuestion) socket.emit('nextQuestion', currentQuestion);
+        players[socket.id] = { username: username, score: 0 };
+        if (Object.keys(players).length === 1 && !currentQuestion) {
+            chargerNouvelleQuestion();
+        } else if (currentQuestion) {
+            socket.emit('nextQuestion', currentQuestion);
+        }
+        const sorted = Object.values(players).sort((a, b) => b.score - a.score);
+        io.emit('updateLeaderboard', sorted);
     });
-    socket.on('disconnect', () => { delete players[socket.id]; });
+
+    socket.on('submitAnswer', (data) => {
+        if (players[socket.id]) {
+            if (data.isCorrect) {
+                players[socket.id].score += 150;
+            } else {
+                players[socket.id].score = Math.max(0, players[socket.id].score - 50);
+            }
+            socket.emit('yourScore', players[socket.id].score);
+            const sorted = Object.values(players).sort((a, b) => b.score - a.score);
+            io.emit('updateLeaderboard', sorted);
+        }
+    });
+
+    socket.on('disconnect', () => {
+        if (players[socket.id]) {
+            delete players[socket.id];
+            const sorted = Object.values(players).sort((a, b) => b.score - a.score);
+            io.emit('updateLeaderboard', sorted);
+        }
+    });
 });
 
 const PORT = process.env.PORT || 3000;
